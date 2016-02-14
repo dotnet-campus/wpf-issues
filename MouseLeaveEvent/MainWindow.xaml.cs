48ac007010e535fd497e50701a8828294af6d5f9
王东杰 <wangdongjie@cvte.com>
2016-02-14 14:18:21 +0800

    添加不会触发MouseLeave事件的Demo

---
+++ MouseLeaveEvent/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace MouseLeaveEvent
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1 {Owner = this};
+            window1.ShowDialog();
+        }
+    }
+}
---
+++ MouseLeaveEvent/Window1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MouseLeaveEvent
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("Enter");
+        }
+
+        private void UIElement_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("Leave");
+        }
+    }
+}
