4ab8bafcb1ed67a479b988aefd2bbd6f11198ec5
lindexi <lindexi_gd@163.com>
2020-03-12 09:15:37 +0800

    ChildWindowDisplayedAndThenBackToMainWindow

---
+++ ChildWindowDisplayedAndThenBackToMainWindow/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ChildWindowDisplayedAndThenBackToMainWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = new Window()
+            {
+                
+            };
+
+            window.Show();
+        }
+    }
+}
