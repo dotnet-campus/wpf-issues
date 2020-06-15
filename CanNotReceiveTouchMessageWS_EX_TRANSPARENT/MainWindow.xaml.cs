025e4e8f19a7bcd9efabd5f9dca74ecd09a87a87
lindexi <lindexi_gd@163.com>
2020-06-15 21:18:39 +0800

    WPF can not receive the touch message when set the WS_EX_TRANSPARENT win32

---
+++ CanNotReceiveTouchMessageWS_EX_TRANSPARENT/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanNotReceiveTouchMessageWS_EX_TRANSPARENT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MouseDown += MainWindow_MouseDown;
+            TouchDown += MainWindow_TouchDown;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr window, int index, int value);
+
+        private const int GWL_EXSTYLE = -20;
+
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr window, int index);
+
+        private void MainWindow_TouchDown(object sender, TouchEventArgs e)
+        {
+            Log("Touch down");
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Log("Mouse down");
+        }
+
+        private void Log(string message)
+        {
+            TextBlock.Text += $"{DateTime.Now} {message} \r\n";
+        }
+    }
+}
